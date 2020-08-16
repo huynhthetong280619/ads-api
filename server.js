c3f9b241f95994495be73337ec3c2c6cca3fe3ff
huynhthetong280619 <huynhthetong1999999@gmail.com>
2020-08-16 22:23:16 +0700

    Add lib cors

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-
+app.use(cors())
 
 const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
